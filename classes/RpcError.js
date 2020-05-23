7a44c05cc4d3182889fcaad8c87d61eae7a1bb07
Alex Corn <alex@alexcorn.com>
2020-05-23 19:35:22 -0400

    Added RpcError class

---
+++ classes/RpcError.js
@@ -0,0 +1,15 @@
+class RpcError extends Error {
+	/**
+	 *
+	 * @param {string} message
+	 * @param {number} code - MUST be an integer
+	 * @param {object|null} data - Optional data to send along with the error
+	 */
+	constructor(message, code, data = null) {
+		super(message);
+		this.code = code;
+		this.data = data;
+	}
+}
+
+module.exports = RpcError;
---
+++ index.js
@@ -0,0 +1,1 @@
+exports.RpcError = require('./classes/RpcError.js');
