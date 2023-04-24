c36f5c06595f9bb823bc8e2d6a316d3f8150af91
Alex Corn <alex@alexcorn.com>
2023-04-24 16:47:18 -0400

    Added BadGateway, Unauthorized, Forbidden close codes

--- src/enums/WebSocketStatusCode.ts
+++ src/enums/WebSocketStatusCode.ts
@@ -1,3 +1,5 @@
+// https://www.iana.org/assignments/websocket/websocket.xhtml#close-code-number
+
 enum WebSocketStatusCode {
 	NormalClosure = 1000,         /** Graceful disconnection */
 	EndpointGoingAway = 1001,     /** Closing connection because either the server or the client is going down (e.g. browser navigating away) */
@@ -13,8 +15,12 @@ enum WebSocketStatusCode {
 	UnexpectedCondition = 1011,   /** Server is terminating because it encountered an unexpected condition that prevented it from fulfilling the request */
 	ServiceRestart = 1012,        /** Server is terminating because it is restarting. */
 	TryAgainLater = 1013,         /** Server is terminating because of some temporary condition, e.g. it is overloaded. */
-	//Reserved2 = 1014,           /** Reserved. Do not use. */
-	TLSFailed = 1015              /** MUST NOT be sent over the wire. Used internally when TLS handshake fails. */
+	BadGateway = 1014,            /** Server is acting as a gateway and received an invalid response from the upstream server. */
+	TLSFailed = 1015,             /** MUST NOT be sent over the wire. Used internally when TLS handshake fails. */
+	Unauthorized = 3000,
+	Forbidden = 3003
+
+	// 4000-4999 may be used for app-specific close codes.
 }
 
 export default WebSocketStatusCode;
